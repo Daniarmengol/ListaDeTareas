36e7322dc21efb3386e459caf18c18cb480c9e3f
Dani Fernández Armengol <danielito486@gmail.com>
2022-06-06 22:31:24 +0200

    Buscador finalizado

--- js/data.js
+++ js/data.js
@@ -1,11 +1,11 @@
 const tareas = new Array();
 document.getElementById('add').addEventListener('click', addTarea);
-ocument.getElementById('inputBuscarTarea').addEventListener('click', buscadorTareas);
-ocument.getElementById('buscarPorPrioridad').addEventListener('click', buscadorTareas);
+document.getElementById('inputBuscarTarea').addEventListener('input', buscadorTareas);
+document.getElementById('buscarPorPrioridad').addEventListener('change', buscadorTareas);
 
 //Esta función debe crear una nueva tarea según lo que contenga el input y el select, y ñluego. añadirla al array de tareas
 function addTarea() {
-    console.log(document.getElementById('inputNuevaTarea'));
+
     const tarea = {
         id: tareas.length + 1,
         nombre: document.getElementById('inputNuevaTarea').value,
@@ -13,16 +13,17 @@ function addTarea() {
     };
 
     tareas.push(tarea);
-    pintarTareas();
+    document.getElementById('inputNuevaTarea').value = '';
+    pintarTareas(tareas);
 
 }
 
 //Función nueva para pintar el array
 
-function pintarTareas() {
+function pintarTareas(pList) {
     const ul = document.getElementById('misTareas');
     ul.innerHTML = "";
-    tareas.forEach(tarea => {
+    pList.forEach(tarea => {
 
         let li = document.createElement('li');
         li.classList.add(tarea.prioridad);
@@ -39,7 +40,32 @@ function pintarTareas() {
     })
 }
 
-function buscadorTareas{
+function buscadorTareas() {
+    //Recoger los value del input y el select y asignarlos a nuevas constantes
+    const inputData = document.getElementById('inputBuscarTarea').value;
+    const selectData = document.getElementById('buscarPorPrioridad').value;
+
+    //Crear un nuevo array que su valor sea el array tareas según lo que quiera filtrar el usuario
+    // ejemplo: tareas.filter()
+
+    let tareasFiltered = [];
+    if (inputData !== '' || selectData !== 'all') {
+        tareasFiltered = tareas.filter(tarea => inputData !== '' ? tarea.nombre.toLowerCase().includes(inputData.toLowerCase()) : true || selectData !== 'all' ? tarea.prioridad == selectData : true)
+        console.log(tareasFiltered);
+    } else {
+        tareasFiltered = tareas;
+    }
+
+
+    /*  if(selectData === 'all'){
+         
+     } */
+
+    pintarTareas(tareasFiltered);
+
+
+    //Pintar las tareas ya filtrada
+    //ejemplo: pintarTareas(nuevoArray);
 
 
 }
